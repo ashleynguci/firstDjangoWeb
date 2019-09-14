74f0ed1da08afe1fe92e193960836fba66ce1fee
ashleynguci <trucanhftu2@gmail.com>
2019-09-14 14:03:41 +0300

    modified setting debug to false

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z)$lvxni)i8yag&xe#)gmj2@08h!&wj82&ay9)1=w=b6qc6&!4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
